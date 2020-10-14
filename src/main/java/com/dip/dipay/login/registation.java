23a9919402388efb1143f13020163f4559064173
Dipayan Manna <54394778+dipayan000@users.noreply.github.com>
2020-10-14 11:58:29 +0530

    Login
    It's an Android Login page

---
+++ src/main/java/com/dip/dipay/login/registation.java
@@ -0,0 +1,13 @@
+package com.dip.dipay.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registation);
+    }
+}
